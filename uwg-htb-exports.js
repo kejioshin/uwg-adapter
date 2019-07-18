75bfd422de0bf269cd0975982442f3f252f317e9
kejioshin <47820853+kejioshin@users.noreply.github.com>
2019-07-18 18:34:01 -0400

    added new files to branch 2

---
+++ uwg-htb-exports.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var shellInterface;
+var __directInterface;
+var SpaceCamp;
+//? if (FEATURES.GPT_LINE_ITEMS) {
+shellInterface.UWGHtb = {
+    render: SpaceCamp.services.RenderService.renderDfpAd.bind(null, 'UWGHtb')
+};
+
+/* Existing creatives use window.pbjs.renderApnxAd */
+window.pbjs = window.pbjs || {};
+window.pbjs.renderApnxAd = SpaceCamp.services.RenderService.renderDfpAd.bind(null, 'UWGHtb');
+//? }
+if (__directInterface.Layers.PartnersLayer.Partners.UWGHtb) {
+    shellInterface.UWGHtb = shellInterface.UWGHtb || {};
+    shellInterface.UWGHtb.adResponseCallback
+    = __directInterface.Layers.PartnersLayer.Partners.UWGHtb.adResponseCallback;
+}
